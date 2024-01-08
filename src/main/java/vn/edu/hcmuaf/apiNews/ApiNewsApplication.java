308c28342a49a5037938f9fa7e60818457c69bab
hoang20130267 <20130267@st.hcmuaf.edu.vn>
2024-01-08 09:15:24 +0700

    .

--- src/main/java/vn/edu/hcmuaf/apiNews/ApiNewsApplication.java
+++ src/main/java/vn/edu/hcmuaf/apiNews/ApiNewsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiNewsApplication {
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiNewsApplication.class, args);
